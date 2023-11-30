0a5e72d14da3a77e45329187efb08f12e0e2a041
d4ve101011 <125325797+d4ve101011@users.noreply.github.com>
2023-12-01 02:32:02 +0800

    up

--- client/src/components/Chat/Landing.tsx
+++ client/src/components/Chat/Landing.tsx
@@ -22,7 +22,7 @@ export default function Landing({ Header }: { Header?: ReactNode }) {
       <div className="flex h-full flex-col items-center justify-center">
         <div className="mb-3 h-[72px] w-[72px]">
           <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black">
-            <img src="/assets/hyprlab_logo.png" alt="HyprLab Logo" />
+            <img src="/assets/hyprlab_icon.png" alt="HyprLab Logo" />
           </div>
         </div>
         <div className="mb-5 text-2xl font-medium dark:text-white">
