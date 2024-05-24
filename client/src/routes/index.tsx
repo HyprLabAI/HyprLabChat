c01cb844d4c676f729e7411d4222358db3606752
d4ve101011 <125325797+d4ve101011@users.noreply.github.com>
2024-05-24 22:09:30 +0800

    hypr_up

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,66 @@
+import { createBrowserRouter, Navigate, Outlet } from 'react-router-dom';
+import {
+  Login,
+  Registration,
+  RequestPasswordReset,
+  ResetPassword,
+  ApiErrorWatcher,
+} from '~/components/Auth';
+import { AuthContextProvider } from '~/hooks/AuthContext';
+import ShareRoute from './ShareRoute';
+import ChatRoute from './ChatRoute';
+import Search from './Search';
+import Root from './Root';
+
+const AuthLayout = () => (
+  <AuthContextProvider>
+    <Outlet />
+    <ApiErrorWatcher />
+  </AuthContextProvider>
+);
+
+export const router = createBrowserRouter([
+  {
+    path: 'register',
+    element: <Registration />,
+  },
+  {
+    path: 'forgot-password',
+    element: <RequestPasswordReset />,
+  },
+  {
+    path: 'reset-password',
+    element: <ResetPassword />,
+  },
+  {
+    path: 'share/:shareId',
+    element: <ShareRoute />,
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: '/',
+        element: <Root />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="/c/new" replace={true} />,
+          },
+          {
+            path: 'c/:conversationId?',
+            element: <ChatRoute />,
+          },
+          {
+            path: 'search',
+            element: <Search />,
+          },
+        ],
+      },
+    ],
+  },
+]);
